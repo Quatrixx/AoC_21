dc4244f61cfed00e9b976515f7f32a16aaa945d3
Alexander Kitzig <alexander.kitzig@gmx.de>
2021-12-26 00:41:16 +0100

    Update day 7 code to include part 2 solution

--- day7_the_treachery_of_whales/the_treachery_of_whales.py
+++ day7_the_treachery_of_whales/the_treachery_of_whales.py
@@ -1,14 +1,29 @@
-def try_position(start_positions, target_pos):
+def try_position(start_positions, target_pos, fuel_use_is_nonlinear=False):
     used_fuel = 0
-    for c in start_positions:
-        used_fuel += abs(c - target_pos)
+    if fuel_use_is_nonlinear:
+        for c in start_positions:
+            used_fuel += sum(range(abs(c - target_pos)+1))
+    else:
+        for c in start_positions:
+            used_fuel += abs(c - target_pos)
     return used_fuel
 
 with open('day7.in') as input:
     crabs = [int(f) for f in input.readline().strip().split(',')]
 
+# oart one
 fuel_values = []
 for p in range(min(crabs), max(crabs)+1):
     fuel_values.append(try_position(crabs, p))
+print(f"part 1, linear fuel use:")
 print(f"most_efficient_pos @ {fuel_values.index(min(fuel_values))}")
-print(f"fuel @ that pos = {min(fuel_values)}")
+print(f"fuel_for_mep = {min(fuel_values)}")
+print()
+
+# part two
+fuel_values = []
+for p in range(min(crabs), max(crabs)+1):
+    fuel_values.append(try_position(crabs, p, fuel_use_is_nonlinear=True))
+print(f"part 2, factorial fuel use:")
+print(f"most_efficient_pos @ {fuel_values.index(min(fuel_values))}")
+print(f"fuel_for_mep = {min(fuel_values)}")
