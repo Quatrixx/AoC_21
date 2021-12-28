4a29d2850d2bba8104ba18c64843a6462e46c487
Alexander Kitzig <alexander.kitzig@gmx.de>
2021-12-28 07:58:36 +0100

    Clean up day 9 solution with lessons from day 11

--- day09_smoke_basin/smoke_basin.py
+++ day09_smoke_basin/smoke_basin.py
@@ -1,34 +1,38 @@
 def get_neighbor_coords(grid: list, c: tuple):
     neighbor_list = []
     x, y = c[0], c[1]
-    if y > 0:
+    north = y > 0
+    west = x > 0
+    east = x < len(grid[y])-1
+    south = y < len(grid)-1
+    if north:
         neighbor_list.append((x, y-1))
-    if x > 0:
+    if west:
         neighbor_list.append((x-1, y))
-    if x < len(grid)-1:
+    if east:
         neighbor_list.append((x+1, y))
-    if y < len(grid[x])-1:
+    if south:
         neighbor_list.append((x, y+1))
     return neighbor_list
     
 def get_neighbor_heights(grid: list, c: tuple):
     neighbor_coords = get_neighbor_coords(grid, c)
-    return [grid[x][y] for (x, y) in neighbor_coords]
+    return [grid[y][x] for (x, y) in neighbor_coords]
 
 def check_if_lowpoint(grid: list, c: tuple):
     x, y = c[0], c[1]
     neighbors = sorted(get_neighbor_heights(grid, (x, y)))
-    return (grid[x][y] < neighbors[0])
+    return (grid[y][x] < neighbors[0])
 
 def check_basin_size_recursively(input_map, output_map, c: tuple):
     neighbor_coords = get_neighbor_coords(input_map, c)
     x, y = c[0], c[1]
-    output_map[x][y] = 1
+    output_map[y][x] = 1
     size = 1
     for (n_x, n_y) in neighbor_coords:
-        if output_map[n_x][n_y] == 0:
-            if input_map[n_x][n_y] == 9:
-                output_map[n_x][n_y] = 9
+        if output_map[n_y][n_x] == 0:
+            if input_map[n_y][n_x] == 9:
+                output_map[n_y][n_x] = 9
             else:
                 size += check_basin_size_recursively(input_map, output_map, (n_x, n_y))
     return size
@@ -39,8 +43,8 @@ low_point_list = []
 
 # part one
 risk_level_sum = 0
-for x, row in enumerate(heightmap):
-    for y, cell in enumerate(row):
+for y, row in enumerate(heightmap):
+    for x, cell in enumerate(row):
         is_lowpoint = check_if_lowpoint(heightmap, (x, y))
         if is_lowpoint:
             low_point_list.append((x,y))
